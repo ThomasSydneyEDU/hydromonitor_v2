9e6c1e9d8fc64c1e0ec368e5fe188ff3fece6f14
ThomasSydneyEDU <ThomasSydneyEDU@users.noreply.github.com>
2025-07-13 08:15:24 +1000

    Update index.js

--- webserver/functions/index.js
+++ webserver/functions/index.js
@@ -25,7 +25,16 @@ exports.getStatus = functions.https.onRequest((req, res) => {
         return;
       }
 
-      res.json(snapshot.docs[0].data());
+      const rawData = snapshot.docs[0].data();
+      const data = {
+        ...rawData,
+        "Air Temp (Indoor)": Number(rawData["Air Temp (Indoor)"]),
+        "Air Temp (Outdoor)": Number(rawData["Air Temp (Outdoor)"]),
+        "Humidity (Indoor)": Number(rawData["Humidity (Indoor)"]),
+        "Humidity (Outdoor)": Number(rawData["Humidity (Outdoor)"]),
+      };
+
+      res.json(data);
     } catch (error) {
       res.status(500).json({error: error.message});
     }
@@ -52,10 +61,10 @@ exports.getHistory = functions.https.onRequest((req, res) => {
           "timestamp": data.timestamp ?
             data.timestamp.toDate().toISOString() :
             null,
-          "Air Temp (Indoor)": data["Air Temp (Indoor)"],
-          "Air Temp (Outdoor)": data["Air Temp (Outdoor)"],
-          "Humidity (Indoor)": data["Humidity (Indoor)"],
-          "Humidity (Outdoor)": data["Humidity (Outdoor)"],
+          "Air Temp (Indoor)": Number(data["Air Temp (Indoor)"]),
+          "Air Temp (Outdoor)": Number(data["Air Temp (Outdoor)"]),
+          "Humidity (Indoor)": Number(data["Humidity (Indoor)"]),
+          "Humidity (Outdoor)": Number(data["Humidity (Outdoor)"]),
         };
       });
 
